f6a97e291acff302305ad7d60df092693d99e939
Aayansh <aayanshj2024@gmail.com>
2025-08-19 01:40:33 +0000

    I mean nothing to commit

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,25 @@
+
 import type {NextConfig} from 'next';
 
 const isGithubActions = process.env.GITHUB_ACTIONS === 'true'
-const repoName = '/clock' // IMPORTANT: This is set to your repository name.
 
-const nextConfig: NextConfig = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
   output: 'export',
-  basePath: isGithubActions ? repoName : '',
-  assetPrefix: isGithubActions ? repoName : '',
-  /* config options here */
+  // This is the name of your repository.
+  basePath: isGithubActions ? '/clock' : '',
+  assetPrefix: isGithubActions ? '/clock/' : '',
+  // Your other Next.js config options here
+  images: {
+    unoptimized: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      }
-    ],
   },
 };
 
